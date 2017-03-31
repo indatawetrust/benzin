e3a12e971f40bd74c17c7af6d56ede9993af60f6
Ahmet Simsek <iamaroott@gmail.com>
2017-04-01 02:52:09 +0300

    test

--- test.js
+++ test.js
@@ -3,25 +3,23 @@ import supertest from 'supertest'
 import faker from 'faker'
 import { models } from './benzin.json'
 import _ from 'lodash'
+import e from 'elek'
 
-let schemas = ['User']
+let schemas = []
 
-for (let i in models) {
-  for (let j in models[i]) {
-    if (models[i][j]) {
-      if (models[i][j].ref) {
-        switch (models[i][j].relation) {
-          case 'hasMany':
-              schemas.push(i)
-            break
-          case 'belongsTo':
-              schemas.push(i)
-            break
-        }
-      }
-    }
+schemas = _.map(models, (key, model) => {
+  return {
+    model,
+    refs: e(key, 'ref')
   }
-}
+})
+
+_.map(schemas.slice(), (s,i) => {
+  if (s.refs.map(r => r.relation).indexOf('hasMany') === -1) {
+    schemas.splice(i, 1)
+    schemas.push(s)
+  }
+})
 
 const request = supertest('http://localhost:3000')
 
@@ -73,4 +71,84 @@ test('user sign in', async t => {
 
   t.is(status, 200)
 
+})
+
+_.map(schemas, s => {
+
+  let { model } = s,
+      Data = {}
+  
+  model = model.toLowerCase()
+
+  test(`${model} authorization test`, async t => {
+
+    const res = await request
+                      .post(`/${model}`)
+                      .send({
+                        text: faker.lorem.words(5)
+                      }),
+          { status } = res
+
+    t.is(status, 401)
+
+  })
+
+  test(`${model} validation test`, async t => {
+
+    const res = await request
+                      .post(`/${model}`)
+                      .send({
+                        text: "************"
+                      })
+                      .set('Authorization', `Bearer ${user.token}`),
+          { status } = res
+
+    t.is(status, 400)
+
+  })
+
+  test(`${model} save test`, async t => {
+
+    const res = await request
+                      .post(`/${model}`)
+                      .send({
+                        text: "helloworldaa"
+                      })
+                      .set('Authorization', `Bearer ${user.token}`),
+          { status, body } = res,
+          { data } = body
+
+    Data = data
+
+    t.is(status, 200)
+
+  })
+
+  test(`${model} get test`, async t => {
+
+    const res = await request
+                      .get(`/${model}/${Data.id}`)
+                      .set('Authorization', `Bearer ${user.token}`),
+          { status } = res
+
+    t.is(status, 200)
+
+  })
+
+  test(`${model} update test`, async t => {
+
+    const res = await request
+                      .put(`/${model}/${Data.id}`)
+                      .send({
+                        text: "adsasadsdasdadad"
+                      })
+                      .set('Authorization', `Bearer ${user.token}`),
+          { status, body } = res,
+          { data } = body
+
+    t.notDeepEqual(data, Data)
+    t.is(status, 200)
+
+  })
+  
 })
